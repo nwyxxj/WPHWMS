ba00745a82a782b7ca5a45c4238d6ef5d19d327b
nwyxxj <nwy.xxj@gmail.com>
2014-09-10 14:48:13 +0800

    Admin Lecturer Student

--- rmesh/src/java/entity/Admin.java
+++ rmesh/src/java/entity/Admin.java
@@ -11,5 +11,27 @@ package entity;
  * @author weiyi.ngow.2012
  */
 public class Admin {
-    
+    private String adminID;
+    private String adminPassword;
+
+    public Admin(String adminID, String adminPassword) {
+        this.adminID = adminID;
+        this.adminPassword = adminPassword;
+    }
+
+    public String getAdminID() {
+        return adminID;
+    }
+
+    public void setAdminID(String adminID) {
+        this.adminID = adminID;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }        
 }
