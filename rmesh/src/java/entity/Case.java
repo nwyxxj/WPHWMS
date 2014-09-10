88200c0308652771832066f11b61dee495ec2684
gkhp92 <hpkhoo.2012@sis.smu.edu.sg>
2014-09-10 14:48:36 +0800

    Case entity
    creation of case entity

---
+++ rmesh/src/java/entity/Case.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entity;
+
+/**
+ *
+ * @author hpkhoo.2012
+ */
+public class Case {
+    
+    private int caseID;
+    private String caseName;
+    private String caseDescription;
+    private String status;
+    private String admissionInfo;
+
+    public int getCaseID() {
+        return caseID;
+    }
+
+    public String getCaseName() {
+        return caseName;
+    }
+
+    public String getCaseDescription() {
+        return caseDescription;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getAdmissionInfo() {
+        return admissionInfo;
+    }
+
+    public void setCaseID(int caseID) {
+        this.caseID = caseID;
+    }
+
+    public void setCaseName(String caseName) {
+        this.caseName = caseName;
+    }
+
+    public void setCaseDescription(String caseDescription) {
+        this.caseDescription = caseDescription;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setAdmissionInfo(String admissionInfo) {
+        this.admissionInfo = admissionInfo;
+    }
+    
+    
+         
+}
